89db099b104b3f1f4657b53b457ef7591c09ecaf
Christian Geib <christian.geib79@gmail.com>
2018-10-11 11:49:35 +0100

    7th commit latest data added to data loader

--- src/main/java/com/codeclan/coursebookingservice/components/DataLoader.java
+++ src/main/java/com/codeclan/coursebookingservice/components/DataLoader.java
@@ -1,5 +1,6 @@
 package com.codeclan.coursebookingservice.components;
 
+import com.codeclan.coursebookingservice.models.Booking;
 import com.codeclan.coursebookingservice.models.Course;
 import com.codeclan.coursebookingservice.models.Customer;
 import com.codeclan.coursebookingservice.repository.bookings.BookingRepository;
@@ -43,6 +44,16 @@ public class DataLoader implements ApplicationRunner {
 
         Customer customer3 = new Customer("William", "Glasgow", 40);
         customerRepository.save(customer3);
+
+        Booking booking1 = new Booking("24-10-18", course1, customer1);
+        bookingRepository.save(booking1);
+
+        Booking booking2 = new Booking("24-10-18", course1, customer3);
+        bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("01-12-18", course3, customer2);
+        bookingRepository.save(booking3);
+
 
     }
 }
--- src/main/java/com/codeclan/coursebookingservice/models/Booking.java
+++ src/main/java/com/codeclan/coursebookingservice/models/Booking.java
@@ -44,4 +44,20 @@ public class Booking {
     public void setDate(String date) {
         this.date = date;
     }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
